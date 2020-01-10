8fc64791df2f16d7bb2cceb11bca0f7d464792d2
Trinity Terry <trinitycterry@gmail.com>
2020-01-10 08:47:49 -0600

    first commit

--- README.md
+++ README.md
@@ -1,1 +1,14 @@
 # nss-manipulating-dom-ux-ux-challenge
+
+Date Created: 01/10/20 | Date Modified: 01/10/20
+
+[![Netlify Status](https://api.netlify.com/api/v1/badges/6a6e5627-4348-457b-8c2e-ea86d4a08473/deploy-status)](https://app.netlify.com/sites/trinity-manipulating-dom/deploys)
+
+This assignment is practice for Book 2: Chapter 1 of NSS's course work. Link to the assignment: https://github.com/nashville-software-school/client-side-mastery/blob/master/book-2-the-neophyte/chapters/IDENTIFYING_DOM_COMPONENTS.md
+
+Preview Assignment: https://trinity-manipulating-dom.netlify.com/
+*** 
+
+This assignment uses JavaScript to manipulate html elements by adding classes with querySelector().
+
+The challange instruction was to find a blog online and try to make my webpage look like that blog.
---
+++ script.js
@@ -0,0 +1,25 @@
+/* Use JavaScript to obtain a reference to the first section with the class of article_header and change its text with the textContent property to "Welcome to the {insert your name here} blog"
+*/
+
+const articleHeader = document.querySelector(".article__header");
+
+articleHeader.innerHTML = "Welcome to the Trinity blog";
+
+
+/* Use JavaScript to obtain a reference to all article__header elements and change their classList property value to article__header important.
+ */
+const allArticleHeaders = document.querySelector(".article__header");
+
+allArticleHeaders.classList.add("important");
+
+/* Obtain a reference the element with a class of dashed and remove it.
+ */
+
+const dashClass = document.querySelector(".dashed");
+
+ dashClass.classList.remove("dashed");
+
+/* Obtain a reference the element with a class of article_footer and add the class of goldenrod it. */
+const footerClass = document.querySelector(".article__footer");
+
+footerClass.classList.add("goldenrod");
